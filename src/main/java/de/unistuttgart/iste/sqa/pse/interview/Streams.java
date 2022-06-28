87d093898d9422f290ef2f34b6d9cb99185803bd
nk-coding <niklask.coding@gmail.com>
2022-06-28 15:06:19 +0200

    simple stream exercise

---
+++ src/main/java/de/unistuttgart/iste/sqa/pse/interview/Streams.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.iste.sqa.pse.interview;
+
+import java.util.List;
+
+public class Streams {
+
+    public static void main(String[] args) {
+        final List<Integer> numbers = List.of(2, 4, 7, 9, 16, 6, 127, 8, 100);
+        // Using streams: filter for even numbers, order by value and then print each number to the console
+    }
+
+}
